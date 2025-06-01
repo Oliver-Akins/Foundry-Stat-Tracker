965cb26b51164c2a10b8840fb0788d953668c1ab
Oliver-Akins <Oliver.akins1999@gmail.com>
2025-06-01 11:25:10 -0600

    Update the tests import not to be bundled for production

--- module/main.mjs
+++ module/main.mjs
@@ -8,7 +8,6 @@ import "./hooks/ready.mjs";
 import "./hooks/preCreateChatMessage.mjs";
 
 // Dev Only imports
-import "./__tests__/registration.mjs";
-// if (import.meta.env.DEV) {
-// 	import(`./__tests__/registration.mjs`);
-// }
+if (import.meta.env.DEV) {
+	import(`./__tests__/registration.mjs`);
+};
