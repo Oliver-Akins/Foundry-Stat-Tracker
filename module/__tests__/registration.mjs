a72c33b9011cd00cd9d2a7a47c0dbdf7fd3c052c
Oliver-Akins <Oliver.akins1999@gmail.com>
2025-06-12 19:27:20 -0600

    Rename files to better indicate that they're tests

--- module/__tests__/registration.mjs
+++ module/__tests__/registration.mjs
@@ -1,8 +1,8 @@
-import { barGraphTests } from "./schemas/barGraph.mjs";
-import { numberBucketTests } from "./schemas/numberBucket.mjs";
-import { rowTests } from "./schemas/row.mjs";
-import { stringBucketTests } from "./schemas/stringBucket.mjs";
-import { tableTests } from "./schemas/table.mjs";
+import { barGraphTests } from "./schemas/barGraph.test.mjs";
+import { numberBucketTests } from "./schemas/numberBucket.test.mjs";
+import { rowTests } from "./schemas/row.test.mjs";
+import { stringBucketTests } from "./schemas/stringBucket.test.mjs";
+import { tableTests } from "./schemas/table.test.mjs";
 
 Hooks.on(`quenchReady`, (quench) => {
 	numberBucketTests(quench);
